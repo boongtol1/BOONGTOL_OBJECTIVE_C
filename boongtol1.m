15630ab4dfebc4c1ca5c1faf00f91135639570c8
boongtol1 <boongtol@icloud.com>
2024-08-12 23:37:19 +0900

    boongtol

---
+++ boongtol1.m
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+@interface SampleClass:NSObject
+- (void)sampleMethod;
+@end
+@implementation SampleClass
+- (void)sampleMethod{
+NSLog(@"Hello, World! \n");
+}
+@end
---
+++ boongtol2.m
@@ -0,0 +1,7 @@
+int main()
+{
+/* my first program in Objective-C */
+SampleClass *sampleClass = [[SampleClass alloc]init];
+[sampleClass sampleMethod];
+return 0;
+}
